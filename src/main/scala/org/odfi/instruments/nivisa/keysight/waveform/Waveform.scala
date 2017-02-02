21c4b7d7c85a166886501180bc4754f17d71e487
richnou <leys.richard@gmail.com>
2017-02-03 00:25:19 +0100

    Added base for Compression support of output data

--- src/main/scala/org/odfi/instruments/nivisa/keysight/waveform/Waveform.scala
+++ src/main/scala/org/odfi/instruments/nivisa/keysight/waveform/Waveform.scala
@@ -1,15 +1,21 @@
 package org.odfi.instruments.nivisa.keysight.waveform
 
-import java.io.{DataOutputStream, File, FileOutputStream}
+import java.io.{ DataOutputStream, File, FileOutputStream }
 
 import scala.collection.mutable.ArrayBuffer
 import java.io.DataInputStream
 import com.idyria.osi.tea.io.TeaIOUtils
 import java.io.FileInputStream
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorOutputStream
+import java.util.zip.ZipFile
+import java.util.zip.ZipOutputStream
+import java.util.zip.GZIPOutputStream
+import org.odfi.instruments.compress.CompressModule
 
 /**
-  * Created by Tristran on 01.02.2017.
-  */
+ * Created by Tristran on 01.02.2017.
+ */
 class Waveform(val preamble: Preamble) {
 
   var values: Option[Array[Double]] = None
@@ -29,32 +35,34 @@ class Waveform(val preamble: Preamble) {
           byte => byte.toDouble
         }.toArray)
 
-
       case Preamble.FORMAT_ASCII =>
     }
 
   }
-  
+
   def getValues = values match {
     case Some(data) => data
     case None => throw new RuntimeException("Waveform has not been initialised with data")
   }
-  def setValues(arr:Array[Double]) = {
+  def setValues(arr: Array[Double]) = {
     this.values = Some(arr)
   }
 
   def getPointsCount = {
     this.getValues.size
   }
-  
+
   def toBinaryFile(f: File) = {
+
+    //-- Check compression format
+    var fileOutStream = CompressModule.getFileCompressOutputStream(f)
 
     if (f.exists()) {
       f.delete()
     }
 
     // Open Binary Stream
-    var os = new DataOutputStream(new FileOutputStream(f))
+    var os = new DataOutputStream(fileOutStream)
 
     // Write FIrst line with preamble
     os.writeChars(preamble.prString)
@@ -63,52 +71,47 @@ class Waveform(val preamble: Preamble) {
     // Write next line with bytes
     this.values.get.foreach(os.writeDouble(_))
 
-
     os.flush
     os.close
-   
+
   }
 
 }
 
-
 object Waveform {
-  
-  def fromBinaryFile(f:File) = {
-    
-    
-    
+
+  def fromBinaryFile(f: File) = {
+
     //-- Open
-    var is = new DataInputStream(new FileInputStream(f))
+    
+    var is = new DataInputStream(CompressModule.getFileCompressInputStream(f))
 
     //-- Take first line
-     //-- First Line is preamble
+    //-- First Line is preamble
     var preamble = new String
     var c = is.readChar()
-    while (c!='\n') {
-      
+    while (c != '\n') {
+
       preamble += c
-      
+
       c = is.readChar()
     }
-    
+
     //var preamble = is.readLine().trim()
- 
-    println("Read PR: "+preamble)
-    
+
+    println("Read PR: " + preamble)
+
     //-- Prepare
     var waveform = new Waveform(new Preamble(preamble))
-    
+
     //-- Get Data
-    println("Size: "+waveform.preamble.points.toInt)
-    var data = (0 until waveform.preamble.points.toInt).map (i => is.readDouble()).toArray
+    println("Size: " + waveform.preamble.points.toInt)
+    var data = (0 until waveform.preamble.points.toInt).map(i => is.readDouble()).toArray
     waveform.setValues(data)
-    
-    
+
     is.close()
     waveform
-    
-    
+
   }
-  
+
 }
